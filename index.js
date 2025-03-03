c0e598f02cc2637c17531dc29a9dfc9844007f6e
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-03-03 15:47:38 +0600

    login page connected to home page

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("index.js")
+const phoneNumber = document.getElementById('login-phone-number')
+const pinNumber = document.getElementById('login-pin-number')
+document.getElementById('login-button').addEventListener('click', function () {
+    const phoneDigit = phoneNumber.value;
+    const pinDigit = pinNumber.value;
+    console.log(phoneDigit, pinDigit)
+    if (phoneDigit === "1357" && pinDigit === "1234") {
+        window.location.href = "home.html"
+    }
+    else {
+        alert('Wrong Information')
+    }
+})

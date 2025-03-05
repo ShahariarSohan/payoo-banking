8bd12cfde3899494dc947f75fae60b6680b72f20
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-03-05 15:01:50 +0600

    some transaction features added

--- Js/addMoney.js
+++ Js/addMoney.js
@@ -8,6 +8,7 @@ document.getElementById('add-money-button').addEventListener('click', function (
     let accountNumber = getValue("add-money-account-number")
     let pinNumber = getValue("add-money-pin-number")
     let amount = getValue("add-money-amount");
+    console.log(accountNumber, pinNumber, amount)
     if (accountNumber === 1234 && pinNumber === 1234 && isNaN(amount) === false) {
         const balance = getBalance("balance")
         const newBalance = balance + amount;
--- Js/transaction.js
+++ Js/transaction.js
@@ -1,16 +1,20 @@
 // transaction card
 document.getElementById("transaction").addEventListener('click', function () {
     hideForm("transaction-container")
+    let addMoneyAmount = getValue("add-money-amount");
+    let cashOutAmount = getValue("cash-out-amount");
+    console.log(addMoneyAmount, cashOutAmount)
     const transaction = document.createElement('div')
     transaction.innerHTML = `
-    <div">
-        <div class="divider"></div>
-        <h1 class="font-bold">Add amount :</h1>
-        <div class="divider"></div>
-        <h1 class="font-bold">Cash out :</h1>
-    </div>
+    <h1>
+     Amount :${addMoneyAmount}
+    </h1>
     `
     document.getElementById("transaction-history").appendChild(transaction)
-
+    // delete button
+    document.getElementById("delete-button").addEventListener('click', function () {
+        // document.getElementById("transaction-history").remove();
+        transaction.remove()
+    })
 })
-// delete button
+

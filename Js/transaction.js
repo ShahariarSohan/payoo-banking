88293413ff16bd6eb4d6ac0a55b805141e3b135b
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-03-05 12:09:48 +0600

    some transaction features added

--- Js/transaction.js
+++ Js/transaction.js
@@ -1,3 +1,16 @@
+// transaction card
 document.getElementById("transaction").addEventListener('click', function () {
     hideForm("transaction-container")
+    const transaction = document.createElement('div')
+    transaction.innerHTML = `
+    <div">
+        <div class="divider"></div>
+        <h1 class="font-bold">Add amount :</h1>
+        <div class="divider"></div>
+        <h1 class="font-bold">Cash out :</h1>
+    </div>
+    `
+    document.getElementById("transaction-history").appendChild(transaction)
+
 })
+// delete button
